d5cd05c67fa456f2a7a48d4319eb85a5e9f1181e
Chris Allen <chris@awesometouch.org>
2012-01-15 13:41:04 -0800

    Updated shit

--- assets/js/blfm.js
+++ assets/js/blfm.js
@@ -8,19 +8,41 @@ var BL = (function () {
 		var map = new L.Map(map_div_id);
 		
 		var tileUrl =
-		    'http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
+			'http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
 		var tileAttrib = 
 		    '© OpenStreetMap contributors, CC-BY-SA, ' +
 		    'Tiles Courtesy of MapQuest';
 		var tiles = new L.TileLayer(
 		    tileUrl, 
 		    {
-		        maxZoom: 19, 
+		        maxZoom: 23, 
 		        attribution: tileAttrib,
 		    }
 		);
-		map.setView(new L.LatLng(0,0), 13).addLayer(tiles);
+		map.addLayer(tiles);
+		                
+		map.on('locationfound', onLocationFound);
+		map.on('locationerror', onLocationError);
 		
+		map.locateAndSetView(16);
+		
+		function onLocationFound(e) {
+			var radius = e.accuracy / 2;
+
+			var marker = new L.Marker(e.latlng);
+			map.addLayer(marker);
+			marker.bindPopup("You are within " + radius + " meters from this point").openPopup();
+			
+			var circle = new L.Circle(e.latlng, radius);
+			map.addLayer(circle);
+		}
+		
+		function onLocationError(e) {
+			alert(e.message);
+		}
+		
+		//map.setView(new L.LatLng(0,0), 13).addLayer(tiles);
+		/*
 		var markerLocation = new L.LatLng(51.5, -0.09),
 			marker = new L.Marker(markerLocation);
 		
@@ -44,18 +66,21 @@ var BL = (function () {
 		
 		polygon.bindPopup("I am a polygon.");
 		map.addLayer(polygon);
+		*/
+		
 		
 		map.on('click', onMapClick);
 		
 		var popup = new L.Popup();
 				
 		function onMapClick(e) {
-			var latlngStr = '(' + e.latlng.lat.toFixed(3) + ', ' + e.latlng.lng.toFixed(3) + ')';
+			var latlngStr = '(' + e.latlng.lat + ', ' + e.latlng.lng + ')';
 			
 			popup.setLatLng(e.latlng);
 			popup.setContent("You clicked the map at " + latlngStr);
 			map.openPopup(popup);
 		}
+		
 	}
 	
 	return {
